f74ec1a4fb20b79f5b7fc49fdcb1c2ccb345bd4b
Ahsan Habib <ahnazir.ahn@gmail.com>
2023-06-05 18:47:28 +0600

    assignment

---
+++ MigrationAssignment/database/migrations/2023_06_05_062353_create_products_table.php
@@ -0,0 +1,29 @@
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    public function up()
+{
+    Schema::create('products', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->decimal('price');
+        $table->text('description');
+        $table->integer('quantity')->nullable();
+        $table->timestamps();
+    });
+}
+
+public function down()
+{
+    Schema::dropIfExists('products');
+}
+
+
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
